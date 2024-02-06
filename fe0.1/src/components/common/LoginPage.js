ef456d69fd9c5c02ba4b558b854c2a96da7ac261
Keegan Farkas <kwfarkas42@tntech.edu>
2024-02-06 16:25:22 -0600

    added email, name, and farm location to registration

--- fe0.1/src/components/common/LoginPage.js
+++ fe0.1/src/components/common/LoginPage.js
@@ -7,9 +7,13 @@ class LoginPage extends Component {
     super(props);
     // sets username, password, and password confirmation to empty values
     this.state = {
+      strEmail: "",
+      strFirstName: "",
+      strLastName: "",
       strUsername: "",
       strPassword: "",
       strConfirmPassword: "",
+      strCity: "",
     };
   }
 
@@ -24,9 +28,14 @@ class LoginPage extends Component {
   };
 
   clearFields = () => {
+    this.setState({ strEmail: "" });
+    this.setState({ strFirstName: "" });
+    this.setState({ strLastName: "" });
     this.setState({ strUsername: "" });
     this.setState({ strPassword: "" });
     this.setState({ strConfirmPassword: "" });
+    this.setState({ strCity: "" });
+    this.setState({ strState: "" });
   };
 
   validatePasswords = () => {
@@ -133,10 +142,10 @@ class LoginPage extends Component {
 
   render() {
     return (
-      <section className="vh-100% gradient-custom">
+      <section className="vh-100 gradient-custom">
         <div className="container py-5 h-100">
           <div className="row d-flex justify-content-center align-items-center h-100">
-            <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div className="col-12 col-md-9 col-lg-7 col-xl-6 col-xxl-5">
               <div id="cardLogin" className="card bg-dark text-white">
                 <div className="card-body p-5 text-center">
                   <div className="mb-md-5 mt-md-4 pb-5">
@@ -172,7 +181,9 @@ class LoginPage extends Component {
                         required
                       />
                       <p className="small">
-                        <a className="text-white-50" href="#forgotPass">Forgot password?</a>
+                        <a className="text-white-50" href="#forgotPass">
+                          Forgot password?
+                        </a>
                       </p>
                       <button
                         className="btn btn-outline-light btn-lg px-5 mt-5 pt-lg-2"
@@ -216,7 +227,7 @@ class LoginPage extends Component {
 
               <div id="cardRegister" className="card bg-dark text-white">
                 <div className="card-body p-5 text-center">
-                  <div className="mb-md-5 mt-md-4 pb-5">
+                  <div className=" mt-md-4 mb-5">
                     <h2 className="fw-bold mb-2 text-uppercase">FarmFolio</h2>
                     <p className="text-white-50 mb-5">Welcome To FarmFolio!</p>
 
@@ -228,6 +239,39 @@ class LoginPage extends Component {
                         type="text"
                         id="txtRegistrationEmail"
                         className=" form-control-lg"
+                        placeholder="Email"
+                        aria-label="Email"
+                        value={this.state.strEmail}
+                        name="strEmail" // add a name attribute to the input
+                        onChange={this.handleInputChange}
+                        required
+                      />
+                      <input
+                        type="text"
+                        id="txtRegistrationFirstName"
+                        className=" form-control-lg mt-2"
+                        placeholder="First Name"
+                        aria-label="First Name"
+                        value={this.state.strFirstName}
+                        name="strFirstName" // add a name attribute to the input
+                        onChange={this.handleInputChange}
+                        required
+                      />
+                      <input
+                        type="text"
+                        id="txtRegistrationLastName"
+                        className=" form-control-lg mt-2"
+                        placeholder="Last Name"
+                        aria-label="Last Name"
+                        value={this.state.strLastName}
+                        name="strLastName" // add a name attribute to the input
+                        onChange={this.handleInputChange}
+                        required
+                      />
+                      <input
+                        type="text"
+                        id="txtRegistrationUsername"
+                        className=" form-control-lg mt-2"
                         placeholder="Username"
                         aria-label="Username"
                         value={this.state.strUsername}
@@ -253,11 +297,87 @@ class LoginPage extends Component {
                         placeholder="Confirm Password"
                         aria-label="Confirm Password"
                         value={this.state.strConfirmPassword}
-                        s
                         name="strConfirmPassword" // add a name attribute to the input
                         onChange={this.handleInputChange}
                         required
                       />
+                      <p className="text-white-50 mt-2">Farm Location</p>
+                      <div className="form-row">
+                        <input
+                          type="text"
+                          id="txtRegistrationCity"
+                          className="form-control-lg mt-2"
+                          placeholder="City"
+                          aria-label="City"
+                          value={this.state.strCity}
+                          name="strCity" // add a name attribute to the input
+                          onChange={this.handleInputChange}
+                          required
+                        />
+                        <select
+                          id="txtRegistrationState"
+                          className="form-control-lg ms-2"
+                          aria-label="State"
+                          value={this.state.strState}
+                          name="strState" // add a name attribute to the input
+                          onChange={this.handleInputChange}
+                          required
+                        >
+                          <option value="" disabled>
+                            State
+                          </option>
+                          <option value="AL">AL</option>
+                          <option value="AK">AK</option>
+                          <option value="AZ">AZ</option>
+                          <option value="AR">AR</option>
+                          <option value="CA">CA</option>
+                          <option value="CO">CO</option>
+                          <option value="CT">CT</option>
+                          <option value="DE">DE</option>
+                          <option value="FL">FL</option>
+                          <option value="GA">GA</option>
+                          <option value="HI">HI</option>
+                          <option value="ID">ID</option>
+                          <option value="IL">IL</option>
+                          <option value="IN">IN</option>
+                          <option value="IA">IA</option>
+                          <option value="KS">KS</option>
+                          <option value="KY">KY</option>
+                          <option value="LA">LA</option>
+                          <option value="ME">ME</option>
+                          <option value="MD">MD</option>
+                          <option value="MA">MA</option>
+                          <option value="MI">MI</option>
+                          <option value="MN">MN</option>
+                          <option value="MS">MS</option>
+                          <option value="MO">MO</option>
+                          <option value="MT">MT</option>
+                          <option value="NE">NE</option>
+                          <option value="NV">NV</option>
+                          <option value="NH">NH</option>
+                          <option value="NJ">NJ</option>
+                          <option value="NM">NM</option>
+                          <option value="NY">NY</option>
+                          <option value="NC">NC</option>
+                          <option value="ND">ND</option>
+                          <option value="OH">OH</option>
+                          <option value="OK">OK</option>
+                          <option value="OR">OR</option>
+                          <option value="PA">PA</option>
+                          <option value="RI">RI</option>
+                          <option value="SC">SC</option>
+                          <option value="SD">SD</option>
+                          <option value="TN">TN</option>
+                          <option value="TX">TX</option>
+                          <option value="UT">UT</option>
+                          <option value="VT">VT</option>
+                          <option value="VA">VA</option>
+                          <option value="WA">WA</option>
+                          <option value="WV">WV</option>
+                          <option value="WI">WI</option>
+                          <option value="WY">WY</option>
+                        </select>
+                      </div>
                       <button
                         className="btn btn-outline-light btn-lg px-5 mt-4 pt-lg-2"
                         type="submit"

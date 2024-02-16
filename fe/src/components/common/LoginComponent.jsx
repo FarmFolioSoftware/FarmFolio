374ea517a6829abd7e5a0d6885a69e804f565fb6
DHatchie <hatcher.dylan02@gmail.com>
2024-02-15 23:44:48 -0600

    Login and Registration pages revamped

--- fe/src/components/common/LoginComponent.jsx
+++ fe/src/components/common/LoginComponent.jsx
@@ -5,7 +5,7 @@ class LoginComponent extends Component {
     return (
       <div className="card bg-dark text-white">
         <div className="card-body p-5 text-center">
-          <div className="mb-md-5 mt-md-4 pb-5">
+          <div className="mb-md-3 mt-md-3 pb-4">
             <h2 className="fw-bold mb-2 text-uppercase">FarmFolio</h2>
             <p className="text-white-50 mb-5">Please enter your login and password!</p>
 

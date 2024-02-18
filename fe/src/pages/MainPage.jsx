45a02704658851ef2832b2f26d9774510d42c6c8
Keegan Farkas <kwfarkas42@tntech.edu>
2024-02-18 08:12:06 -0600

    added tabs and some other small layout changes

--- fe/src/pages/MainPage.jsx
+++ fe/src/pages/MainPage.jsx
@@ -3,6 +3,7 @@
 import React, { Component } from "react";
 import withRouter from "../components/withRouter";
 import "../assets/styles/main.css";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
 class MainPage extends Component {
   constructor(props) {
@@ -42,11 +43,11 @@ class MainPage extends Component {
   render() {
     return (
       <div className="min-height-100vh gradient-custom d-flex flex-column justify-content-between">
-        <nav className="col-12 d-flex justify-content-between">
+        <nav className="col-12 d-flex justify-content-between position-relative">
           <div className="bg-dark px-5 rounded-br d-flex align-items-center">
             <h1 className="text-white m-0 lh-1 py-2">FarmFolio</h1>
           </div>
-          <div className="bg-dark rounded-br rounded-bl h-100 px-5 py-2 d-flex align-items-center">
+          <div className="bg-dark rounded-br rounded-bl h-75 px-5 py-2 d-flex align-items-center center">
             <p className="text-white m-0 px-5">Online Status</p>
           </div>
           <div className="bg-dark px-3 rounded-bl d-flex align-items-center">
@@ -59,30 +60,64 @@ class MainPage extends Component {
             </a>
           </div>
         </nav>
-        <div className="d-flex my-5">
+
+        <div className="d-flex my-4 align-items-center">
           <div className="col-3">
-            <div
-              className="card bg-dark col-11 mx-auto"
-              style={{ height: "400px" }}
-            ></div>
+            <div className="card bg-dark col-11 mx-auto p-3">
+              <h2 className="text-white text-center mb-4">Profile Info</h2>
+              <p className="text-white">Current Farm:</p>
+              <p className="text-white">User:</p>
+              <p className="text-white">Clocked In?:</p>
+              {/*Placeholder*/}
+              <img
+                className="mx-1 mb-5 mt-3"
+                src="/images/login-reg-bg.jpg"
+                alt="Image of your farm"
+              />
+            </div>
           </div>
           <div className="col-6">
-            <div
-              className="card bg-dark col-11 mx-auto"
-              style={{ height: "400px" }}
-            ></div>
+            <Tabs>
+              <TabList>
+                <Tab>Plots</Tab>
+                <Tab>Actions</Tab>
+                <Tab>Time</Tab>
+              </TabList>
+
+              <TabPanel>
+                <div className="card bg-dark p-5">
+                  {/*Placeholder*/}
+                  <img src="/images/login-reg-bg.jpg" alt="" />
+                </div>
+              </TabPanel>
+              <TabPanel>
+                <div className="card bg-dark p-5">
+                  {/*Placeholder*/}
+                  <img src="/images/farmfolioLogo.png" alt="" />
+                </div>
+              </TabPanel>
+              <TabPanel>
+                <div className="card bg-dark p-5">
+                  {/*Placeholder*/}
+                  <img src="/images/login-reg-bg.jpg" alt="" />
+                </div>
+              </TabPanel>
+            </Tabs>
           </div>
           <div className="col-3 d-flex flex-column justify-content-between">
-            <div
-              className="card bg-dark col-11 mx-auto"
-              style={{ height: "180px" }}
-            ></div>
-            <div
-              className="card bg-dark col-11 mx-auto"
-              style={{ height: "180px" }}
-            ></div>
+            <div className="card bg-dark col-11 mx-auto p-3 mb-3">
+              <h2 className="text-white text-center">Weather</h2>
+              {/*Placeholder*/}
+              <img src="/images/login-reg-bg.jpg" alt="" />
+            </div>
+            <div className="card bg-dark col-11 mx-auto p-3 mt-3">
+              <h2 className="text-white text-center">Finances</h2>
+              {/*Placeholder*/}
+              <img src="/images/login-reg-bg.jpg" alt="" />
+            </div>
           </div>
         </div>
+
         <footer className="bg-dark py-4">
           <p className="text-center text-white m-0">FarmFolio</p>
         </footer>

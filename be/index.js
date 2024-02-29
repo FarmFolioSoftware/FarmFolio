f086efada6ca02c8eae86cc48605b19b8d0de1ec
Jhoop18 <94129297+Jhoop18@users.noreply.github.com>
2024-02-29 10:28:39 -0600

    updated

--- be/index.js
+++ be/index.js
@@ -235,7 +235,7 @@ app.post("/register", async (req, res) => {
 		
 		await dbConnection.query("INSERT INTO tblDemographics (userID, race, sex, DOB) VALUE (?, ?, ?, STR_TO_DATE(?, '%Y-%m-%d'));", [targetUserID, strRace, strSex, strBirthday]);
 
-		await dbConnection.query("INSERT INTO tblUserRole (userID, roleID) VALUE (?, CASE WHEN ? = 'Farm Owner' THEN 1 WHEN ? = 'Employee' THEN 2 END);", [targetUserID, strRole, strRole]);
+		//await dbConnection.query("INSERT INTO tblUserRole (userID, roleID) VALUE (?, CASE WHEN ? = 'Farm Owner' THEN 1 WHEN ? = 'Employee' THEN 2 END);", [targetUserID, strRole, strRole]);
 		
 		await dbConnection.query("INSERT INTO tblFarm (farmName, addressID) VALUE (?, ?);", [strFarmName, targetAddressID]);
 		

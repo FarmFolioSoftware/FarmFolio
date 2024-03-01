38b7b8ad8d9e7d2b9d0087630f69e6148d445084
Garrett Hayes <garrettbridgeshayes@gmail.com>
2024-02-29 22:11:04 -0600

    format fixed for now

--- fe/src/pages/MainPage.jsx
+++ fe/src/pages/MainPage.jsx
@@ -20,17 +20,17 @@ class MainPage extends Component {
       strDay: "",
       strPlotOptions: [],
       strInOutHTML: [],
-      blnClockedIn: false,
+      blnClockedIn: false
     };
   }
 
-  handleInputChange = (event) => {
+  handleInputChange = event => {
     const target = event.target;
     const name = target.name;
     const value = target.value;
 
     this.setState({
-      [name]: value,
+      [name]: value
     });
   };
 
@@ -49,31 +49,42 @@ class MainPage extends Component {
 
   getWeatherData() {
     //make sure to host backend using node index.js in the backend folder
-    const arrWeekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const arrWeekday = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
     const d = new Date();
 
-    var token = localStorage.getItem('uuidSessionToken');
+    var token = localStorage.getItem("uuidSessionToken");
     fetch("http://3.82.57.93:8000/getWeather?uuidSessionToken=" + token, {
       method: "GET",
       headers: {
-        "Content-Type": "application/json",
-      },
+        "Content-Type": "application/json"
+      }
     })
-      .then((response) => response.json())
-      .then((data) => {
+      .then(response => response.json())
+      .then(data => {
         console.log(data);
 
         // Uncomment the following lines if you want to update the component's state
         this.setState({
           strWeatherDesc: JSON.parse(JSON.stringify(data.weather_description)),
           strWeatherTemp: JSON.parse(JSON.stringify(data.weather_temp)),
-          strWeatherIconURL: "https://openweathermap.org/img/wn/" + JSON.parse(JSON.stringify(data.icon)) + "@2x.png",
+          strWeatherIconURL:
+            "https://openweathermap.org/img/wn/" +
+            JSON.parse(JSON.stringify(data.icon)) +
+            "@2x.png",
           strCity: JSON.parse(JSON.stringify(data.city)),
           strState: JSON.parse(JSON.stringify(data.state)),
-          strDay: arrWeekday[d.getDay()],
+          strDay: arrWeekday[d.getDay()]
         });
       })
-      .catch((error) => {
+      .catch(error => {
         console.error("Error in MainPage.jsx:", error);
         if (error.response && error.response.text) {
           console.log("Raw Response Text:", error.response.text());
@@ -82,107 +93,116 @@ class MainPage extends Component {
       });
   }
 
-  clock = (event) => {
+  clock = event => {
     fetch("http://3.82.57.93:8000/clockButton", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        uuidSessionToken: localStorage.getItem('uuidSessionToken'),
+        uuidSessionToken: localStorage.getItem("uuidSessionToken"),
         clockinout: event.event
-      }),
-    })
-    .then((data) => {
-      console.log(data)
-    })
+      })
+    }).then(data => {
+      console.log(data);
+    });
 
     this.setState({ blnClockedIn: !this.state.blnClockedIn });
-  }
-
+  };
 
   //CALL TO POPULATE PLOT BOX!!! NEEDS BACKEND CALL
   populatePlots = () => {
-    var token = localStorage.getItem('uuidSessionToken');
-    fetch('http://3.82.57.93:8000/listPlots?uuidSessionToken=' + token, {
+    var token = localStorage.getItem("uuidSessionToken");
+    fetch("http://3.82.57.93:8000/listPlots?uuidSessionToken=" + token, {
       method: "GET",
       headers: {
-        "Content-Type": "application/json",
-      },
+        "Content-Type": "application/json"
+      }
     })
-      .then((response) => response.json())
-      .then((data) => {
+      .then(response => response.json())
+      .then(data => {
         let plotOptions = [];
-        data.plots.forEach((plot) => {
-          plotOptions.push(<option key={plot.plotID} value={plot.plotID}>{plot.plotName}</option>);
+        data.plots.forEach(plot => {
+          plotOptions.push(
+            <option key={plot.plotID} value={plot.plotID}>
+              {plot.plotName}
+            </option>
+          );
         });
         this.setState({
           strPlotOptions: plotOptions
         });
-
       });
-
-  }
+  };
 
   populateTime = () => {
-    var token = localStorage.getItem('uuidSessionToken');
-    fetch('http://3.82.57.93:8000/getTimePunches?uuidSessionToken=' + token, {
+    var token = localStorage.getItem("uuidSessionToken");
+    fetch("http://3.82.57.93:8000/getTimePunches?uuidSessionToken=" + token, {
       method: "GET",
       headers: {
-        "Content-Type": "application/json",
-      },
+        "Content-Type": "application/json"
+      }
     })
-      .then((response) => response.json())
-      .then((data) => {
+      .then(response => response.json())
+      .then(data => {
         let strHTML = [];
-        data.punches.forEach((inOut) => {
-          const timeInFormatted = new Date(inOut.timeIn).toLocaleString("en-US", {
-            year: "numeric",
-            month: "2-digit",
-            day: "2-digit",
-            hour: "2-digit",
-            minute: "2-digit",
-            second: "2-digit",
-            hour12: false
-          });
-          const timeOutFormatted = new Date(inOut.timeOut).toLocaleString("en-US", {
-            year: "numeric",
-            month: "2-digit",
-            day: "2-digit",
-            hour: "2-digit",
-            minute: "2-digit",
-            second: "2-digit",
-            hour12: false
-          });
-          strHTML.push(<div className="card col-10 offset-1 bg-dark text-white mt-1"><p>Time In: {timeInFormatted}</p><p>Time Out: {timeOutFormatted}</p></div>);
+        data.punches.forEach(inOut => {
+          const timeInFormatted = new Date(inOut.timeIn).toLocaleString(
+            "en-US",
+            {
+              year: "numeric",
+              month: "2-digit",
+              day: "2-digit",
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+              hour12: false
+            }
+          );
+          const timeOutFormatted = new Date(inOut.timeOut).toLocaleString(
+            "en-US",
+            {
+              year: "numeric",
+              month: "2-digit",
+              day: "2-digit",
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+              hour12: false
+            }
+          );
+          strHTML.push(
+            <div className="card col-10 offset-1 bg-dark text-white mt-1">
+              <p>Time In: {timeInFormatted}</p>
+              <p>Time Out: {timeOutFormatted}</p>
+            </div>
+          );
         });
         this.setState({
-          strInOutHTML: strHTML,
+          strInOutHTML: strHTML
         });
-
       });
-
-  }
+  };
 
   getUserData() {
     //make sure to host backend using node index.js in the backend folder
-    var token = localStorage.getItem('uuidSessionToken');
-    fetch('http://3.82.57.93:8000/getUserInfo?uuidSessionToken=' + token, {
+    var token = localStorage.getItem("uuidSessionToken");
+    fetch("http://3.82.57.93:8000/getUserInfo?uuidSessionToken=" + token, {
       method: "GET",
       headers: {
-        "Content-Type": "application/json",
-      },
+        "Content-Type": "application/json"
+      }
     })
-      .then((response) => response.json())
-      .then((data) => {
+      .then(response => response.json())
+      .then(data => {
         console.log(data);
 
         this.setState({
-          strFarmName: JSON.parse(JSON.stringify(data.farmName)), 
-          strFullName: JSON.parse(JSON.stringify(data.fullName)), 
+          strFarmName: JSON.parse(JSON.stringify(data.farmName)),
+          strFullName: JSON.parse(JSON.stringify(data.fullName))
         });
       })
-      .catch((error) => {
+      .catch(error => {
         console.error("Error in MainPage.jsx:", error);
         if (error.response && error.response.text) {
           console.log("Raw Response Text:", error.response.text());
@@ -191,25 +211,24 @@ class MainPage extends Component {
       });
   }
 
-  logoutCall = (event) => {
+  logoutCall = event => {
     const { navigate } = this.props;
     event.preventDefault();
 
     fetch("http://3.82.57.93:8000/logout", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({
         uuidSessionToken: localStorage.getItem("uuidSessionToken")
-      }),
-    })
-      .then((data) => {
-        console.log(data)
       })
+    }).then(data => {
+      console.log(data);
+    });
     localStorage.removeItem("uuidSessionToken");
     navigate("/");
-  }
+  };
 
   componentDidMount() {
     // Call the function initially
@@ -220,7 +239,6 @@ class MainPage extends Component {
       this.getWeatherData();
     }, 500);
     // this.getWeatherData();
-
 
     // Set up an interval to call the function every 5 seconds
     /*
@@ -237,7 +255,7 @@ class MainPage extends Component {
 
   render() {
     return (
-      <div className="min-height-100vh gradient-custom d-flex flex-column justify-content-between">
+      <div className="min-height-100vh gradient-custom d-flex flex-column">
         <nav className="col-12 d-flex justify-content-between position-relative">
           <div className="bg-dark px-5 rounded-br d-flex align-items-center">
             <h1 className="text-white m-0 lh-1 py-2">FarmFolio</h1>
@@ -250,23 +268,17 @@ class MainPage extends Component {
               Settings
             </a>
             <p className="text-white mx-3 my-0 lh-1">|</p>
-            <a className="text-white text-decoration-none" onClick={this.logoutCall}>
+            <a
+              className="text-white text-decoration-none"
+              onClick={this.logoutCall}
+            >
               Log Out
             </a>
           </div>
         </nav>
 
-        <div className="d-flex my-4 align-items-center">
-          <div className="col-2">
-            <div className="card bg-dark col-11 mx-auto p-3">
-              <h2 className="text-white text-center mb-4">Profile Info</h2>
-              <p className="text-white">{"Farm: " + this.state.strFarmName}</p>
-              <p className="text-white">{"User: " + this.state.strFullName}</p>
-              <button disabled={this.state.blnClockedIn} className="btn btn-outline-light col-8 offset-2 clockButton mb-3" onClick={() => this.clock({event:0})}>Clock In</button>
-              <button disabled={!this.state.blnClockedIn} className="btn btn-outline-light col-8 offset-2 clockButton" onClick={() => this.clock({event:1})}>Clock Out</button>
-            </div>
-          </div>
-          <div className="col-7">
+        <div className="d-flex my-4 align-items-center justify-content-center">
+          <div className="col-8">
             <Tabs>
               <TabList>
                 <Tab>Plots</Tab>
@@ -280,13 +292,25 @@ class MainPage extends Component {
                   <hr />
                   <div className="card-body" style={{ display: "inline-flex" }}>
                     <div className="col-3">
-                      <button className="btn btn-outline-light col-12 mb-3">Add Plot</button>
-                      <select className="form-select plotSelectBox text-white" multiple aria-label="Plots">
+                      <button className="btn btn-outline-light col-12 mb-3">
+                        Add Plot
+                      </button>
+                      <select
+                        className="form-select plotSelectBox text-white"
+                        multiple
+                        aria-label="Plots"
+                      >
                         {this.state.strPlotOptions}
                       </select>
                     </div>
                     <div className="col-9">
-                      <div className="card bg-dark text-white col-11 offset-1" style={{ outline: "white solid 2px", display: "inline-flex" }}>
+                      <div
+                        className="card bg-dark text-white col-11 offset-1"
+                        style={{
+                          outline: "white solid 2px",
+                          display: "inline-flex"
+                        }}
+                      >
                         <div className="col-3 offset-1 mt-2">
                           <p>Plot Name</p>
                           <p>Plot Size: </p>
@@ -294,9 +318,10 @@ class MainPage extends Component {
                           <p>End Date: </p>
                           <p>Last Updated: </p>
                           <p>Description: </p>
-                          <button className="btn btn-outline-light mb-3">Delete Plot</button>
+                          <button className="btn btn-outline-light mb-3">
+                            Delete Plot
+                          </button>
                         </div>
-                        
                       </div>
                     </div>
                   </div>
@@ -304,20 +329,28 @@ class MainPage extends Component {
               </TabPanel>
               <TabPanel>
                 <div className="card bg-dark p-5 text-white">
-                <h1>Actions</h1>
+                  <h1>Actions</h1>
                   <hr />
                   <div className="card-body" style={{ display: "inline-flex" }}>
                     <div className="col-3">
-                      <button className="btn btn-outline-light col-12 mb-3">Add Action</button>
-                      <select className="form-select plotSelectBox text-white" multiple aria-label="Actions">
-
-                      </select>
+                      <button className="btn btn-outline-light col-12 mb-3">
+                        Add Action
+                      </button>
+                      <select
+                        className="form-select plotSelectBox text-white"
+                        multiple
+                        aria-label="Actions"
+                      ></select>
                     </div>
                     <div className="col-9">
-                      <div className="card bg-dark text-white col-11 offset-1" style={{ outline: "white solid 2px", display: "inline-flex" }}>
-                        <div className="col-3 offset-1 mt-2">
-                          
-                        </div>
+                      <div
+                        className="card bg-dark text-white col-11 offset-1"
+                        style={{
+                          outline: "white solid 2px",
+                          display: "inline-flex"
+                        }}
+                      >
+                        <div className="col-3 offset-1 mt-2"></div>
                       </div>
                     </div>
                   </div>
@@ -329,7 +362,13 @@ class MainPage extends Component {
                   <hr />
                   <div className="card-body" style={{ display: "inline-flex" }}>
                     <div className="col-12">
-                      <div className="card bg-dark text-white col-12" style={{ outline: "white solid 2px", display: "inline-flex" }}>
+                      <div
+                        className="card bg-dark text-white col-12"
+                        style={{
+                          outline: "white solid 2px",
+                          display: "inline-flex"
+                        }}
+                      >
                         <div className="col-12 my-2 timeSheet">
                           <div className="divInOut">
                             {this.state.strInOutHTML}
@@ -343,37 +382,65 @@ class MainPage extends Component {
             </Tabs>
           </div>
           <div className="col-3 d-flex flex-column justify-content-between">
-            <div className="card bg-dark col-11 mx-auto p-3 mb-3">
+            <div className="card bg-dark col-11 mx-auto p-3 mt-5">
+              <h2 className="text-white text-center mb-4">Profile Info</h2>
+              <p className="text-white">{"Farm: " + this.state.strFarmName}</p>
+              <p className="text-white">{"User: " + this.state.strFullName}</p>
+              <button
+                disabled={this.state.blnClockedIn}
+                className="btn btn-outline-light col-8 offset-2 clockButton mb-3"
+                onClick={() => this.clock({ event: 0 })}
+              >
+                Clock In
+              </button>
+              <button
+                disabled={!this.state.blnClockedIn}
+                className="btn btn-outline-light col-8 offset-2 clockButton"
+                onClick={() => this.clock({ event: 1 })}
+              >
+                Clock Out
+              </button>
+            </div>
+            <div className="card bg-dark col-11 mx-auto p-3 mb-2 mt-4">
               <h2 className="text-white text-center">Weather</h2>
               <div className="d-flex justify-content-between px-2 mt-2">
                 <div>
-                  <p className="text-white fw-light">{this.state.strWeatherDesc}</p>
+                  <p className="text-white fw-light">
+                    {this.state.strWeatherDesc}
+                  </p>
                   <img src={this.state.strWeatherIconURL} alt="" />
                 </div>
                 <div className="d-flex">
                   <hr className="vr me-4"></hr>
                   <div>
                     <div className="mb-2">
-                      <p className="text-white m-0">{this.state.strCity + ", " + this.state.strState}</p>
-                      <p className="text-white m-0 small fw-light">{this.state.strDay}</p>
+                      <p className="text-white m-0">
+                        {this.state.strCity + ", " + this.state.strState}
+                      </p>
+                      <p className="text-white m-0 small fw-light">
+                        {this.state.strDay}
+                      </p>
                     </div>
-                    <p className="text-white h2">{this.state.strWeatherTemp + " °F"}</p>
+                    <p className="text-white h2">
+                      {this.state.strWeatherTemp + " °F"}
+                    </p>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="card bg-dark col-11 mx-auto p-3 mt-3">
-              <h2 className="text-white text-center">Finances</h2>
-              {/*Placeholder*/}
-              <img src="/images/login-reg-bg.jpg" alt="" />
-            </div>
           </div>
+        </div>
+
+        <div className="card bg-dark col-11 mx-auto p-3 mt-3 mb-3">
+          <h2 className="text-white text-center">Finances</h2>
+          {/*Placeholder*/}
+          <img src="/images/login-reg-bg.jpg" alt="" />
         </div>
 
         <footer className="bg-dark py-4">
           <p className="text-center text-white m-0">FarmFolio</p>
         </footer>
-      </div >
+      </div>
     );
   }
 }

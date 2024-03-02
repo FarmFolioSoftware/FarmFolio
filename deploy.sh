b837eeadd5cc458b3c7149318abc43f3768b719f
Evan Burch <edburch42@tntech.edu>
2024-03-02 00:50:16 -0600

    fix pathing

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,19 @@ pid=$1
 git pull
 
 # Backend
+cd be
 kill -INT $pid
 npm install
 logFileName=log-$(date +"%Y-%m-%d-%H:%M:%S")
 touch /home/ec2-user/logs/$logFileName
 node index.js > /home/ec2-user/logs/$logFileName
+cd ..
 
 # Frontend
-cd /home/ec2-user/FarmFolio/fe
+cd fe
 sudo systemctl stop httpd
+npm install
 npm run build
 sudo mv dist/ /var/www/html/
 sudo systemctl start httpd
+cd ..

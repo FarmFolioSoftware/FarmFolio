da703a3248df85c183f6c8299041abab2bcc9391
Evan Burch <edburch42@tntech.edu>
2024-03-02 01:35:20 -0600

    more logging

--- deploy.sh
+++ deploy.sh
@@ -11,9 +11,9 @@ node index.js > /home/ec2-user/logs/$logFileName
 cd ..
 
 # Frontend
-cd fe > /home/ec2-user/logs/be-$logFileName
-npm install > /home/ec2-user/logs/be-$logFileName
-npm run build > /home/ec2-user/logs/be-$logFileName
-sudo rm -r /var/www/html/dist/ > /home/ec2-user/logs/be-$logFileName
-sudo mv dist/ /var/www/html/ > /home/ec2-user/logs/be-$logFileName
-cd .. > /home/ec2-user/logs/be-$logFileName
+cd fe
+npm install
+npm run build
+sudo rm -r /var/www/html/dist/
+sudo mv dist/ /var/www/html/
+cd ..

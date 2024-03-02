7659caf130dbb4a00912e3e5881de2d29d088e9d
Evan Burch <edburch42@tntech.edu>
2024-03-02 01:56:59 -0600

    add pwds

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,23 @@ logFileName=be-$(date +"%Y-%m-%d-%H:%M:%S")
 git pull
 
 # Frontend
+pwd
 cd fe
+pwd
 npm install
+pwd
 npm run build
 sudo rm -r /var/www/html/dist/
 sudo mv dist/ /var/www/html/
+pwd
 cd ..
 
 # Backend
+pwd
 cd be
 kill -INT $pid
+pwd
 npm install
+pwd
 node index.js > /home/ec2-user/logs/$logFileName
 cd ..

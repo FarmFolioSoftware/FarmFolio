b6c853793ffd79c41c3e9cc0de84f802f0e3f5e5
Evan Burch <edburch42@tntech.edu>
2024-03-02 01:52:05 -0600

    fe first

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,17 @@ pid=$1
 logFileName=be-$(date +"%Y-%m-%d-%H:%M:%S")
 git pull
 
-# Backend
-cd be
-kill -INT $pid
-npm install
-node index.js > /home/ec2-user/logs/$logFileName
-cd ..
-
 # Frontend
 cd fe
 npm install
 npm run build
 sudo rm -r /var/www/html/dist/
 sudo mv dist/ /var/www/html/
+cd ..
+
+# Backend
+cd be
+kill -INT $pid
+npm install
+node index.js > /home/ec2-user/logs/$logFileName
 cd ..

853492154816b9a94b7eb16e2a5afd8ebdd5c418
Evan Burch <edburch42@tntech.edu>
2024-03-03 00:51:49 -0600

    change deploy order

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,16 @@ pid=$1
 logFileName=be-$(date +"%Y-%m-%d-%H:%M:%S")
 git pull
 
+# Backend
+pwd
+kill -INT $pid
+pwd
+npm install
+pwd
+node index.js > /home/ec2-user/logs/$logFileName
+pwd
+cd ..
+
 # Frontend
 pwd
 cd fe
@@ -10,17 +20,8 @@ pwd
 npm install
 pwd
 npm run build
+pwd
 sudo rm -r /var/www/html/dist/
+pwd
 sudo mv dist/ /var/www/html/
 pwd
-cd ..
-
-# Backend
-pwd
-cd be
-kill -INT $pid
-pwd
-npm install
-pwd
-node index.js > /home/ec2-user/logs/$logFileName
-cd ..

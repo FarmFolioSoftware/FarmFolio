8f54d7195ba85f73decf5235b00c968fb0af56a2
Evan Burch <edburch42@tntech.edu>
2024-03-04 23:45:49 -0600

    final test

--- fe/src/pages/LoginPage.jsx
+++ fe/src/pages/LoginPage.jsx
@@ -11,7 +11,7 @@ class LoginPage extends Component {
 	
     // sets username, password, and password confirmation to empty values
     this.state = {
-      strEmail: "test501",
+      strEmail: "",
       strFirstName: "",
       strLastName: "",
       strPassword: "",

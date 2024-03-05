b02fa81854ae49568397c7f9091888031a9c4859
Evan Burch <edburch42@tntech.edu>
2024-03-04 22:31:16 -0600

    attempt to reverse fe and be deploy order

--- deploy.sh
+++ deploy.sh
@@ -3,21 +3,9 @@ pid=$1
 logFileName=be-$(date +"%Y-%m-%d-%H:%M:%S")
 git pull
 
-# Backend
-pwd
-kill -INT $pid
-pwd
-npm install
-pwd
-node index.js > /home/ec2-user/logs/$logFileName &
-# For the love of god why doesnt this command work. It should work
-# screen -md bash -c 'node index.js > /home/ec2-user/logs/$logFileName'
-pwd
-cd ..
-
 # Frontend
 pwd
-cd fe
+cd ~/FarmFolio/fe
 pwd
 npm install
 pwd
@@ -26,4 +14,15 @@ pwd
 sudo rm -r /var/www/html/dist/
 pwd
 sudo mv dist/ /var/www/html/
+
+# Backend
+pwd
+cd ~/FarmFolio/be
+kill -INT $pid
+pwd
+npm install
+pwd
+node index.js > /home/ec2-user/logs/$logFileName # & # running node as a child process also did not work
+# For the love of god why doesnt this command work. It should work
+# screen -md bash -c 'node index.js > /home/ec2-user/logs/$logFileName'
 pwd
